dc2a6866b417580d6090b4c44437e64318cf728a
dhoouglas <dhoouglas@hotmail.com>
2024-10-14 17:23:17 -0300

    Updating proposal positions

--- resources/views/components/proposals/position.blade.php
+++ resources/views/components/proposals/position.blade.php
@@ -1,4 +1,4 @@
-@props(['position'])
+@props(['position', 'status'])
 
 
 <div @class(['flex items-center justify-center rounded-full w-[40px] h-[40px]' , 'bg-[#FEF3C7] text-[#F59E0B]'=> $position == 0,
@@ -7,9 +7,16 @@
     'bg-[#F1F5F9]' => $position > 2,
     ])>
 
-    @if ($position < 3)
+    @if ($position
+    < 3)
         <x-ui.icons.trophy class="w-[24px] h-[24px]" />
     @else
+    @if ($status == 'up')
+    <x-ui.icons.arrow-up class="w-[24px] h-[24px]" />
+    @elseif($status == 'down')
+    <x-ui.icons.arrow-down class="w-[24px] h-[24px]" />
+    @else
     <x-ui.icons.minus class="w-[24px] h-[24px]" />
+    @endif
     @endif
 </div>
